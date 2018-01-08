d22666b6645e8630bcc27e0dc8bcd8fb3e954309
M_ELSaghier <thedreamereng@gmail.com>
2018-01-08 07:04:05 +0200

    Fix Exception in arrayList arrayOutOfBoundException

--- gen/src/main/java/com/elsahier/Jokes.java
+++ gen/src/main/java/com/elsahier/Jokes.java
@@ -9,7 +9,7 @@ public class Jokes {
         jokes.add("My dog used to chase people on a bike a lot It got so bad finally I had to take his bike away");
         jokes.add("Anton, do you think I'm a bad mother?\nMy name is Paul");
         jokes.add("Can a kangaroo jump higher than a house? \nOf course a house doesn't jump at all");
-        int randomIndex = (int) (Math.random() * jokes.size()) - 1;
+        int randomIndex = (int) (Math.random() * jokes.size());
         return jokes.get(randomIndex);
     }
 
