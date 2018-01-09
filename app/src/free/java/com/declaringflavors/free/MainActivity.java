674b220328959e328b0909240d01bf87ab0a9774
M_ELSaghier <thedreamereng@gmail.com>
2018-01-09 04:38:37 +0200

    'flavours'

---
+++ app/src/free/java/com/declaringflavors/free/MainActivity.java
@@ -0,0 +1,32 @@
+package com.declaringflavors.free;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.udacity.gradle.builditbigger.AsyncTaskPackage.EndpointsAsyncTask;
+import com.udacity.gradle.builditbigger.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        AdView mAdView = findViewById(R.id.adView);
+        // Create an ad request. Check logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mAdView.loadAd(adRequest);
+    }
+
+    public void tellJoke(View view) {
+        new EndpointsAsyncTask(this).execute();
+    }
+
+}
