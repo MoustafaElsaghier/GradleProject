711addb0c4b1200dc2001a48fc4237858e3ef143
M_ELSaghier <thedreamereng@gmail.com>
2018-01-10 02:40:21 +0200

    Final Commit :D

--- app/src/paid/java/com/declaringflavors/paid/MainActivity.java
+++ app/src/paid/java/com/declaringflavors/paid/MainActivity.java
@@ -2,7 +2,9 @@ package com.declaringflavors.paid;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
+import com.udacity.gradle.builditbigger.AsyncTaskPackage.EndpointsAsyncTask;
 import com.udacity.gradle.builditbigger.R;
 
 public class MainActivity extends AppCompatActivity {
